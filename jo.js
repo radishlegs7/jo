a4c147ec493ac470cf15aa0f57394c0c4826fa60
radishlegs7 <kyliezhy7@gmail.com>
2024-06-03 14:00:11 +0300

    Add files via upload

---
+++ jo.js
@@ -0,0 +1,9 @@
+var text = new createjs.Text(
+    "i'll always be here for you jo\
+    nhappy birthday love !",
+    "bold 30px Arial", "#fff");
+    text.textAlign = "center";
+    text.x = w / 2;
+    text.y = h / 2 - text.
+    getMeasuredLineHeight();
+    StaticRange.addChild(text);
